27cf6cc8d7c2921382b1627194ca38e984e2d86f
Darkflame72 <31436575+Darkflame72@users.noreply.github.com>
2021-01-21 18:10:16 +1300

    fix nox mypy

--- noxfile.py
+++ noxfile.py
@@ -108,7 +108,7 @@ def safety(session: Session) -> None:
 @nox.session(python=python_versions)
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
-    args = session.posargs or ["obsidion", "tests", "docs/conf.py"]
+    args = session.posargs or ["obsidion", "tests"]
     session.install(".")
     session.install("mypy", "pytest")
     session.run("mypy", *args)
