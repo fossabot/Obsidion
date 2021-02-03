c1da5a6c250e6c97fec4c2f43056a55ff3089e26
Darkflame72 <31436575+Darkflame72@users.noreply.github.com>
2021-02-03 20:43:14 +1300

    add first hypixel command

--- obsidion/cogs/hypixel/hypixel.py
+++ obsidion/cogs/hypixel/hypixel.py
@@ -1,10 +1,14 @@
 """Images cog."""
 import logging
 
+import discord
 from discord.ext import commands
 
 from obsidion.core.i18n import cog_i18n
 from obsidion.core.i18n import Translator
+from obsidion.core import get_settings
+
+from hypixel import Hypixel
 
 log = logging.getLogger(__name__)
 
@@ -16,3 +20,23 @@ class Hypixel(commands.Cog):
     def __init__(self, bot) -> None:
         """Init."""
         self.bot = bot
+        self.hypixel = Hypixel(get_settings().HYPIXEL_API_TOKEN)
+
+    @commands.command()
+    async def watchdogstats(self, ctx: commands.Context) -> None:
+        """Get the current watchdog statistics."""
+        await ctx.channel.trigger_typing()
+        data = await self.hypixel.watchdog_stats()
+        embed = discord.Embed(title=_("Watchdog Stats"), colour=self.bot.color)
+        embed.add_field(
+            name=_("Total Bans"), value=f"{(data.watchdog_total + data.staff_total):,}"
+        )
+        embed.add_field(
+            name=_("Watchdog Rolling Daily"), value=f"{data.watchdog_rollingDaily:,}"
+        )
+        embed.add_field(name=_("Staff Total"), value=f"{data.staff_total:,}")
+        embed.add_field(
+            name=_("Staff Rolling Daily"), value=f"{data.staff_rollingDaily:,}"
+        )
+        embed.timestamp = ctx.message.created_at
+        await ctx.send(embed=embed)
